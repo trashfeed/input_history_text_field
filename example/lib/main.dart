b6c56ef062e46a346c4935605715358587fe23f4
trashfeed <info@trashfeed.net>
2020-08-12 18:23:57 +0900

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,8 @@ void main() {
   runApp(MyApp());
 }
 
+final _inputHistoryController = InputHistoryController();
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,49 +21,118 @@ class MyApp extends StatelessWidget {
           title: Text("Sampe"),
         ),
         body: SingleChildScrollView(
-            padding: EdgeInsets.all(50),
-            child: Column(
-              children: <Widget>[
-                /// sample1
-                InputHistoryTextField(
-                  historyKey: "01",
-                ),
+          padding: EdgeInsets.all(50),
+          child: Column(
+            children: <Widget>[
+              /// sample1
+              /// - basic
+              InputHistoryTextField(
+                historyKey: "01",
+              ),
 
-                /// sampe2
-                InputHistoryTextField(
-                  historyKey: "02",
-                  minLines: 6,
-                  maxLines: 10,
-                  limit: 3,
-                  enableHistory: true,
-                  hasFocusExpand: true,
-                  showHistoryIcon: true,
-                  showDeleteIcon: true,
-                  historyIcon: Icons.add,
-                  deleteIcon: Icons.delete,
-                  enableOpacityGradient: false,
-                  listRowDecoration: BoxDecoration(
-                    border: Border(
-                      left: BorderSide(color: Colors.red, width: 3),
-                    ),
-                  ),
-                  listDecoration: BoxDecoration(
-                    color: Colors.white60,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(8),
-                        bottomRight: Radius.circular(8)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 4,
-                        blurRadius: 6,
-                        // offset: Offset(0, 3),
-                      ),
-                    ],
+              /// sampe2
+              /// - customize
+              InputHistoryTextField(
+                historyKey: "02",
+                minLines: 2,
+                maxLines: 10,
+                limit: 3,
+                enableHistory: true,
+                hasFocusExpand: true,
+                showHistoryIcon: true,
+                showDeleteIcon: true,
+                historyIcon: Icons.add,
+                deleteIcon: Icons.delete,
+                enableOpacityGradient: false,
+                listRowDecoration: BoxDecoration(
+                  border: Border(
+                    left: BorderSide(color: Colors.red, width: 3),
                   ),
                 ),
-              ],
-            )),
+                inputHistoryController: _inputHistoryController,
+                listDecoration: BoxDecoration(
+                  color: Colors.white60,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(4),
+                      bottomRight: Radius.circular(4)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 2,
+                        offset: Offset(0, 3)),
+                  ],
+                ),
+                historyIconTheme: IconTheme(
+                  data: IconThemeData(color: Colors.red, size: 12),
+                  child: Icon(Icons.add),
+                ),
+                deleteIconTheme: IconTheme(
+                  data: IconThemeData(color: Colors.blue, size: 12),
+                  child: Icon(Icons.remove_circle),
+                ),
+                listOffset: Offset(0, 5),
+                listTextStyle: TextStyle(fontSize: 30),
+                historyListItemLayoutBuilder: (value, index) {
+                  return InkWell(
+                    onTap: () => _inputHistoryController.select(value.text),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                              margin: const EdgeInsets.only(left: 10.0),
+                              padding: const EdgeInsets.only(left: 10.0),
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  left: BorderSide(
+                                    width: 5.0,
+                                    color: index % 2 == 0
+                                        ? Colors.red
+                                        : Colors.blue,
+                                  ),
+                                ),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    value.textToSingleLine,
+                                    overflow: TextOverflow.ellipsis,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    DateTime.fromMillisecondsSinceEpoch(
+                                            value.createdTime)
+                                        .toUtc()
+                                        .toString(),
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 10,
+                                        color: Theme.of(context).disabledColor),
+                                  ),
+                                ],
+                              )),
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            size: 16,
+                            color: Theme.of(context).disabledColor,
+                          ),
+                          onPressed: () {
+                            _inputHistoryController.remove(value);
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
